09c4137a75c68a82f94ebf6d1837ed84f5bd26c3
MATHEUS DE VASCONCELOS VELOZO <matheus_velozo@estudante.sesisenai.org.br>
2024-09-19 22:06:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>Cabeçalho</header>
+      <aside>Lado esquerdo</aside>
+      <main>Principal</main>
+      <footer>Rodapé</footer>
+    </>
+  );
+}
+
+export default App
